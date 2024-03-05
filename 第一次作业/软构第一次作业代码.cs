6eb23428b24b1f4fd73bd6e08b400699ceab72e5
Cristy-time <143832722+Cristy-time@users.noreply.github.com>
2024-03-05 16:32:57 +0800

    Add files via upload

---
+++ 第一次作业/软构第一次作业代码.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading;
+namespace First_Homework
+{
+    public class CodeFile1
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("你好！我是张皓源");
+            Thread.Sleep(1000);
+        }
+    }
+}
